2b77b4c657c52005d02c1b62d398eec51a9221bf
손찬형 <133100122+sonchanhyeong@users.noreply.github.com>
2023-09-26 20:45:49 +0900

    memo

---
+++ app/src/main/java/com/sonchan/memo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sonchan.memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sonchan.memo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
