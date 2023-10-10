eae4b58fe044ecf385a1bd08d501bcf314a74bf4
손찬형 <133100122+sonchanhyeong@users.noreply.github.com>
2023-10-10 10:48:03 +0900

    memo

--- app/src/main/java/com/sonchan/memo/AddMemo.kt
+++ app/src/main/java/com/sonchan/memo/AddMemo.kt
@@ -1,5 +1,6 @@
 package com.sonchan.memo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sonchan.memo.databinding.ActivityAddMemoBinding
@@ -10,5 +11,11 @@ class AddMemo : AppCompatActivity() {
         binding = ActivityAddMemoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
