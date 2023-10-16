9f0d859f5c56619897d27026e24417aff493f410
손찬형 <133100122+sonchanhyeong@users.noreply.github.com>
2023-10-17 08:42:20 +0900

    memo

--- app/src/main/java/com/sonchan/memo/AddMemo.kt
+++ app/src/main/java/com/sonchan/memo/AddMemo.kt
@@ -3,9 +3,11 @@ package com.sonchan.memo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import com.sonchan.memo.databinding.ActivityAddMemoBinding
 
 class AddMemo : AppCompatActivity() {
+
     private lateinit var binding: ActivityAddMemoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityAddMemoBinding.inflate(layoutInflater)
@@ -16,6 +18,11 @@ class AddMemo : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
 
             startActivity(intent)
+        }
+
+        binding.saveBtn.setOnClickListener {
+            var title:Editable = binding.titleEdit.text
+            var intext:Editable = binding.inEdit.text
         }
     }
 }
